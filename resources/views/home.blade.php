77d0c5e14e05f30dad6614ea0a1eae32142be65a
samuelinux <samuelpereiramachado@gmail.com>
2025-09-30 14:43:10 -0300

    Feat: Adicionado botão pra whatsapp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,16 @@
 @section('subtitle', 'Escolha uma empresa para agendar seus serviços')
 
 @section('content')
-@if($empresas->isEmpty())
-    <div class="alert alert-error">
-        <p><strong>Nenhuma empresa disponível</strong></p>
-        <p>Não há empresas cadastradas no sistema no momento.</p>
-    </div>
-@else
+
     
-@endif
 
 
+<div style="margin-top: 2rem; padding-top: 1rem;">
+    
+    <a href="https://api.whatsapp.com/send/?phone=5528999673526&text=Quero%20conhecer%20o%20sistema%20de%20agendamento!" class="btn btn-secondary color-button "
+    style="background-color: #25d366; border-color: #25d366; text-color: white; font-weight: bold; font-size: 1.2rem; padding: 0.5rem 1rem; border-radius: 0.5rem; text-decoration: none;" target="_blank">
+        Entrar em contato pelo Whatsapp
+    </a>
+</div>
 @endsection
 
